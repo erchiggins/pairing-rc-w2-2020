61ecbb23b6388d200612e2580439407223387f08
Emily Higgins <emilyrchiggins@gmail.com>
2020-01-16 14:30:46 -0500

    reorganized dupe_files into a function

--- dupe_file/main.py
+++ dupe_file/main.py
@@ -2,21 +2,22 @@ import filecmp
 import os
 import itertools
 
-# only compare directories nested within the current location
+# only compare files in the provided directory at the current level
 
-file_names = []
-
-if __name__ == '__main__':
-    top = 'top'
-    for root, dirs, files in os.walk(top, topdown=False):
+def find_duplicates(directory):
+    file_names = []
+    for root, files in os.walk(directory, topdown=False):
         for name in files:
             file_names.append(os.path.join(root,name))
-    print(file_names)
-
     duplicates = []
-    
     # generate tuples with itertools of all pairings
     for f1, f2 in itertools.combinations(file_names, 2):
         if filecmp.cmp(f1,f2):
             duplicates.append((f1,f2))
-    print(duplicates)
+    return duplicates
+
+if __name__ == '__main__':
+    print(find_duplicates('top'))
+    
+    
+    
